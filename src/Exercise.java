d022cfeac0ea70539b6f962fadd9ddd03f9cf380
Martyna Maron <martyna.gla@gmail.com>
2017-06-20 00:11:10 +0100

    Add a new exercise - Amicable Numbers

--- src/Exercise.java
+++ src/Exercise.java
@@ -4,7 +4,9 @@ public enum Exercise {
     PRIME_NUMBER_DETECTOR(2, "Prime Number Detector program"),
     STAR_SQUARE_DRAWER(3, "Speed coding exercise - draw 2 squares out of stars"),
     //https://www.youtube.com/watch?v=c18GjbnZXMw
-    LOCKERS(4, "Placeholder - TED Riddle about Lockers");
+    LOCKERS(4, "Placeholder - TED Riddle about Lockers"),
+    //https://projecteuler.net/problem=21
+    AMICABLE_NUMBERS(5, "Amicable Numbers from Project Euler");
 
     private final int exerciseNumber;
     private final String exerciseDescription;
