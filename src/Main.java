a993784e0d59283cfd4ad6aaa030967cceede183
Martyna Maron <martyna.gla@gmail.com>
2017-06-21 20:17:01 +0100

    Project Euler Problem 21: Find the sum of all amicable numbers below 10000

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import exercises.AmicableNumbers;
 import utils.UserInput;
 
 public class Main {
 
     public static void main(String[] args) {
-//        ExercisesMap exercisesMap = new ExercisesMap();
-//        exercisesMap.printAllExercises();
-//        exercisesMap.runChosenExercise(UserInput.readPickedInteger());
-        AmicableNumbers amicableNumbers = new AmicableNumbers();
-        amicableNumbers.findAllDivisors(2222);
+        ExercisesMap exercisesMap = new ExercisesMap();
+        exercisesMap.printAllExercises();
+        exercisesMap.runChosenExercise(UserInput.readPickedInteger());
     }
 }
--- src/exercises/AmicableNumbers.java
+++ src/exercises/AmicableNumbers.java
@@ -1,20 +1,41 @@
 package exercises;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class AmicableNumbers {
 
-    public void findAllDivisors(int n) {
-        System.out.println("Divisors of " + n);
-        int numberOfDivisors = 0;
+    public int findTheSumOfAllAmicableNumbers() {
+        int sumOfAllAmicableNumbers = 0;
+        Set<Integer> uniqueAmicableNumbers = new LinkedHashSet<>();
+        for (int i = 0; i<10000; i++){
+            int sum1 = findSumOfAllDivisors(i);
+            int sum2 = findSumOfAllDivisors(sum1);
+            if (i == sum2){
+                if (i == sum1){
+                    System.out.println("Number "+i+" divisors sum up to itself!");
+                } else {
+                    uniqueAmicableNumbers.add(i);
+                    uniqueAmicableNumbers.add(sum1);
+                    System.out.println("Amicable numbers are: " + i + " and " + sum1);
+                }
+            }
+        }
+        System.out.println("=====");
+        for (Integer i: uniqueAmicableNumbers){
+            sumOfAllAmicableNumbers += i;
+            System.out.println(i);
+        }
+        return sumOfAllAmicableNumbers;
+    }
+
+    private int findSumOfAllDivisors(int n) {
         int sumOfAllDivisors= 0;
         for (int i=1; i<n; i++){
             if (n % i == 0){
-                System.out.print(i + ", ");
-                numberOfDivisors++;
                 sumOfAllDivisors += i;
             }
         }
-        System.out.println();
-        System.out.println(n + " has "+numberOfDivisors+" divisors");
-        System.out.println("Sum of all divisors: "+sumOfAllDivisors);
+        return sumOfAllDivisors;
     }
 }
