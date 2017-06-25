1e255260ead892f723bd342497102e4db313140d
Martyna Maron <martyna.gla@gmail.com>
2017-06-25 13:20:58 +0100

    Calculate the sums of two diagonals of the matrix

--- src/exercises/DiagonalDifference.java
+++ src/exercises/DiagonalDifference.java
@@ -3,9 +3,10 @@ package exercises;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//Given a square matrix of size N xN , calculate the absolute difference between the sums of its diagonals.
 public class DiagonalDifference {
 
-    private ArrayList<ArrayList<Integer>> matrix = new  ArrayList<>();
+    private ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
     private Scanner input;
 
     public void readNumber() {
@@ -15,12 +16,14 @@ public class DiagonalDifference {
         populateMatrix(rows);
 
         printMatrix(matrix);
+        System.out.println("Sum of one diagonal: " + getSumOfDiagonal(0, matrix));
+        System.out.println("Sum of second diagonal: " + getSumOfDiagonal(matrix.size(), matrix));
     }
 
     private void populateMatrix(int rows) {
         for (int i = 0; i < rows; i++) {
             ArrayList<Integer> row = new ArrayList<>();
-            for (int j = 0; j < rows; j++){
+            for (int j = 0; j < rows; j++) {
                 int number = input.nextInt();
                 System.out.println("Adding " + number);
                 row.add(number);
@@ -30,12 +33,33 @@ public class DiagonalDifference {
     }
 
     private void printMatrix(ArrayList<ArrayList<Integer>> matrix) {
-        for (ArrayList<Integer> list: matrix) {
-            for (int number: list) {
+        for (ArrayList<Integer> list : matrix) {
+            for (int number : list) {
                 System.out.print(number + " ");
             }
             System.out.println();
         }
+    }
+
+    private int getSumOfDiagonal(int start, ArrayList<ArrayList<Integer>> matrix) {
+        ArrayList<Integer> diagonal = new ArrayList<>();
+        if (start == matrix.size()){
+            for (ArrayList<Integer> row : matrix) {
+                diagonal.add(row.get(start-1));
+                start--;
+            }
+        } else {
+            for (ArrayList<Integer> row : matrix){
+                diagonal.add(row.get(start));
+                start++;
+            }
+        }
+        int sum = 0;
+        for (int number : diagonal) {
+            System.out.print(number + " ");
+            sum += number;
+        }
+        return sum;
     }
 
 }
