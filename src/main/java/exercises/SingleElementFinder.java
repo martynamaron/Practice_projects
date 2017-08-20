26d84125537b91f9f783a6d3c0b3c14e666dfc08
Martyna Maron <martyna.gla@gmail.com>
2017-08-20 14:21:09 +0100

    Add Single element in a sorted array exercise

---
+++ src/main/java/exercises/SingleElementFinder.java
@@ -0,0 +1,15 @@
+package exercises;
+
+//https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+//Find a single element in an array of double elements
+//Your solution should run in O(log n) time and O(1) space.
+
+public class SingleElementFinder {
+
+    // [1,1,2,3,3,4,4,8,8]3
+    public int getSingleElement(int[] array) {
+
+        return 0;
+    }
+
+}
