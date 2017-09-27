79a40c607402a8500d679edd0ed6510ce71ac376
martyna.maron <martyna.maron@lloydsbanking.com>
2017-09-27 13:58:28 +0100

    Add Hanoi Towers challenge

---
+++ src/main/java/exercises/HanoiTowers.java
@@ -0,0 +1,14 @@
+package exercises;
+
+/**
+ * The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+
+ Only one disk can be moved at a time.
+ Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack.
+ No disk may be placed on top of a smaller disk.
+
+ With 3 disks, the puzzle can be solved in 7 moves.
+ The minimal number of moves required to solve a Tower of Hanoi puzzle is 2n − 1, where n is the number of disks.
+ */
+public class HanoiTowers {
+}
